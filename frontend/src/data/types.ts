af650802fa53b93c275f6bc878a316210ece0b0a
Heisenberg <nancologist@gmail.com>
2021-03-01 16:58:14 +0100

    Add data.

---
+++ frontend/src/data/types.ts
@@ -0,0 +1,9 @@
+export interface Emoji {
+    emoji: string;
+    name: string;
+    emoji_version: string;
+    skin_tone_support: boolean;
+    skin_tone_support_unicode_version?: string;
+    slug: string;
+    unicode_version: string;
+}
